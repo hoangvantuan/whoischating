ed507cf3bcdabcb36ac2820ba0b80aa4477fe113
shun <hoang_van_tuan@mediado.jp>
2017-10-19 14:15:22 +0900

    init project

--- src/database/connection.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-function connect() {
-  mongoose.connect ('mongodb://localhost:27017/whoischatting', {useMongoClient: 'true'});
-}
-
-module.exports.connect = connect;
-module.exports.mongoose = mongoose;
